7821252945f33a713bf36834b496cb216f1f80df
Himnamnor <3226584200@qq.com>
2025-08-06 10:56:33 +0800

    设置接口过滤，axiosInstance实例，并完成用户接口

--- furukawa/src/api/User.js
+++ furukawa/src/api/User.js
@@ -1,12 +1,32 @@
-import axios from "axios";
+import axiosInstance from "../util/axiosInstance";
 
-const api=axios.create({
-    baseURL:"http://localhost:8080/api/user",
-    timeout:5000,
-})
+
 
 const user_id=localStorage.getItem("user_id");
 
-export const getUserInfo=(user_id)=>{
-    return null
+export const getUserInfo=()=>{
+    return axiosInstance.get(`/user/${user_id}`);
+}
+
+export const registerUser=(user)=>{
+    return axiosInstance.post(`/user/register`,user);
+}
+
+export const login=(user)=>{
+    const msg={
+        userId:null,
+        userName:user.userName,
+        password:user.password,
+        phone:null,
+        location:null,
+        age:null,
+        height:null,
+        weight:null,
+        role:null,
+    };
+    return axiosInstance.post(`/user/login`,msg);
+}
+
+export const setUserInfo=(user)=>{
+    return axiosInstance.put(`/user/${user_id}`,user);
 }
